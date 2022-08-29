717f80f41f120510fc54e0a5409baccb11355086
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 02:07:25 +0100

    setup postslice

---
+++ src/features/posts/postSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    id: 1,
+    title: "Learning Redux toolkit",
+    content: "Ive been looking at this for a while",
+  },
+  {
+    id: 2,
+    title: "Learning React",
+    content: "Ive been looking at this for a while",
+  },
+];
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+});

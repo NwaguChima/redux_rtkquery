14aed8c5bca46ef7c66e6da81bae6b01a5c41a02
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 01:47:45 +0100

    dispatched actions

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -3,7 +3,18 @@ import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./counterSlice";
 
 const Counter = () => {
-  return <div>Counter</div>;
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+    </section>
+  );
 };
 
 export default Counter;

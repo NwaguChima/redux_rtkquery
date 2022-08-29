5b63eb0cf9582d06b265ed45bda3b62a9077bb43
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 03:09:22 +0100

    setup postAuthor comp

---
+++ src/features/posts/PostAuthor.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersSlice";
+
+import React from "react";
+
+const PostAuthor = () => {
+  return <div>PostAuthor</div>;
+};
+
+export default PostAuthor;

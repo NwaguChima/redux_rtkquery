87ca53e011a41abaf357c4612fac0b83740b7639
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 03:19:23 +0100

    completed timeAgo fnc

--- src/features/posts/TimeAgo.jsx
+++ src/features/posts/TimeAgo.jsx
@@ -2,8 +2,18 @@ import { parseIso, formatDistanceToNow } from "date-fns";
 
 import React from "react";
 
-const TimeAgo = () => {
-  return <div>TimeAgo</div>;
+const TimeAgo = ({ timestamp }) => {
+  let timeAgo = "";
+  if (timestamp) {
+    const date = parseIso(timestamp);
+    const timePeriod = formatDistanceToNow(date);
+    timeAgo = `${timePeriod} ago`;
+  }
+  return (
+    <span title={timestamp}>
+      &nbsp; <i>{timeAgo}</i>
+    </span>
+  );
 };
 
 export default TimeAgo;

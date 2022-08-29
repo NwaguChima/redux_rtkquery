1a9bdaec7d9a9cbcd165f4876687461bfeaa3f1c
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 20:11:32 +0100

    linking from excerpts implemented

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -1,14 +1,16 @@
 import { useSelector } from "react-redux";
-import { selectPostById } from "../../features/posts/postsSlice";
+import { seletPostById } from "./postsSlice";
 
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
+import { useParams } from "react-router-dom";
 
 const SinglePostPage = () => {
   // rerieve postId
+  const { postId } = useParams();
 
-  const post = useSelector((state) => selectPostById(state, postId));
+  const post = useSelector((state) => seletPostById(state, Number(postId)));
 
   if (!post) {
     return (

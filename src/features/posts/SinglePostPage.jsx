377a0b1b9555c89b58913b2275b6c566bd109e4c
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 20:23:05 +0100

    edit post form filled

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -5,9 +5,9 @@ import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SinglePostPage = () => {
-  // rerieve postId
   const { postId } = useParams();
 
   const post = useSelector((state) => seletPostById(state, Number(postId)));
@@ -25,6 +25,7 @@ const SinglePostPage = () => {
       <h3>{post.title}</h3>
       <p>{post.body}</p>
       <p className="postCredit">
+        <Link to={`post/edit/${post.id}`}>Edit Post</Link>
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>

1914a685c2190ad1bac044dedfdd98815a8518bf
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 19:43:59 +0100

    updated save post func

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,32 +1,41 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
   const [title, setTitle] = React.useState("");
   const [content, setContent] = React.useState("");
   const [userId, setUserId] = React.useState("");
+  const [addRequestStatus, setAddRequestStatus] = React.useState("idle");
 
   const users = useSelector(selectAllUsers);
+  const dispatch = useDispatch();
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
 
-  const dispatch = useDispatch();
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
 
   const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postAdded(title, content, userId));
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(addNewPost({ title, content, userId })).unwrap();
+
+        setTitle("");
+        setContent("");
+        setUserId("");
+      } catch (error) {
+        console.error("Failed to save the post:", error);
+      } finally {
+        setAddRequestStatus("idle");
+      }
     }
-
-    setTitle("");
-    setContent("");
   };
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
   const userOptions = users.map((user) => (
     <option key={user.id} value={user.id}>

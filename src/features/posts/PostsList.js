661bcaa512f6d4e6f69399fa36a0a5843fc348b0
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 02:12:17 +0100

    rendering posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,7 +2,21 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 const PostsList = () => {
-  return <div>PostsList</div>;
+  const posts = useSelector((state) => state.posts);
+
+  const renderedPosts = posts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+    </article>
+  ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
 };
 
 export default PostsList;

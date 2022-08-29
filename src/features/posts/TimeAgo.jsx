b161f0039b2c95e96fb15c6250ad7ca527331321
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 03:17:08 +0100

    setup timeago comp and added date to reducer

---
+++ src/features/posts/TimeAgo.jsx
@@ -0,0 +1,9 @@
+import { parseIso, formatDistanceToNow } from "date-fns";
+
+import React from "react";
+
+const TimeAgo = () => {
+  return <div>TimeAgo</div>;
+};
+
+export default TimeAgo;
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,18 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
     id: 1,
     title: "Learning Redux toolkit",
     content: "Ive been looking at this for a while",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
   {
     id: 2,
     title: "Learning React",
     content: "Ive been looking at this for a while",
+    date: sub(new Date(), { minutes: 20 }).toISOString(),
   },
 ];
 
@@ -28,6 +31,7 @@ export const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId,
           },
         };

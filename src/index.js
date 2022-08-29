e17f9586668acc2409e6f0f5b9ef4c4eb59fd1fd
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 20:00:04 +0100

    setup routing

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { fetchUsers } from "./features/users/usersSlice";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
 );

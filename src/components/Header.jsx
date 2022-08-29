fc4ab0a8a5c6eea543341058ae7c73337d278a30
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 20:14:24 +0100

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Redux Blog</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/post">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

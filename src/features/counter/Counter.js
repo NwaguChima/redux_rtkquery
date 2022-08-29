5e4ee8558a27dea912d788fd4fdaae0f0728e23b
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 01:45:15 +0100

    setup counter component

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+const Counter = () => {
+  return <div>Counter</div>;
+};
+
+export default Counter;

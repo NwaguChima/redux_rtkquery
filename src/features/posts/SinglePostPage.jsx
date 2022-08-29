3c04594a5ab81f0c6c5b3f588ca80c4e84e414bb
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 19:58:40 +0100

    rendered posts

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -17,4 +17,18 @@ const SinglePostPage = () => {
       </section>
     );
   }
+
+  return (
+    <article>
+      <h3>{post.title}</h3>
+      <p>{post.body}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  );
 };
+
+export default SinglePostPage;

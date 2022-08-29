aaa503e6b166cdf3b5ff978d0abe8bde859c767d
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 03:23:46 +0100

    sorted posts

--- src/features/posts/TimeAgo.jsx
+++ src/features/posts/TimeAgo.jsx
@@ -1,11 +1,11 @@
-import { parseIso, formatDistanceToNow } from "date-fns";
+import { parseISO, formatDistanceToNow } from "date-fns";
 
 import React from "react";
 
 const TimeAgo = ({ timestamp }) => {
   let timeAgo = "";
   if (timestamp) {
-    const date = parseIso(timestamp);
+    const date = parseISO(timestamp);
     const timePeriod = formatDistanceToNow(date);
     timeAgo = `${timePeriod} ago`;
   }

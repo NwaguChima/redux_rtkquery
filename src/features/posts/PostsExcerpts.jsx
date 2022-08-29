d80f427b7c7758a862e852c8709bc668a4a16ea4
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 19:19:06 +0100

    completed post exercpts

--- src/features/posts/PostsExcerpts.jsx
+++ src/features/posts/PostsExcerpts.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import PostAuthor from "./PostAuthor";
+import ReactionButtons from "./ReactionButtons";
+import TimeAgo from "./TimeAgo";
+
 const PostsExcerpts = ({ post }) => {
-  return <div>PostsExcerpts</div>;
+  return (
+    <article>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  );
 };
 
 export default PostsExcerpts;
--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import PostAuthor from "./PostAuthor";
 import {
   selectAllPosts,
   getPostsStatus,
   getPostsError,
   fetchPosts,
 } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
 import ReactionButtons from "./ReactionButtons";
 import TimeAgo from "./TimeAgo";
 
@@ -28,15 +28,7 @@ const PostsList = () => {
     .sort((a, b) => b.date.localeCompare(a.date));
 
   const renderedPosts = orderedPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-    </article>
+    
   ));
 
   return (

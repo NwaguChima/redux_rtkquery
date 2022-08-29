4efc4245feaa4399a13d403f10faf8a9e2b6e79b
NwaguChima <nwaguchima48@gmail.com>
2022-08-29 20:05:58 +0100

    setup major routing with layout wrapping

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 // import Counter from "./features/counter/Counter";
 import AddPostForm from "./features/posts/AddPostForm";
 import PostsList from "./features/posts/PostsList";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <main className="App">
-      {/* <Counter /> */}
-      <AddPostForm />
-      <PostsList />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 

0ca3cf07cb267727cfbc6ad3bed471a961d5d6c8
NwaguChima <nwaguchima48@gmail.com>
2022-08-30 23:53:57 +0100

    imports for userpage

--- src/features/users/UserPage.jsx
+++ src/features/users/UserPage.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUserById } from "./usersSlice";
+import { selectAllPosts } from "../posts/postsSlice";
+import { Link, useParams } from "react-router-dom";
 
 const UserPage = () => {
   return <div>UserPage</div>;

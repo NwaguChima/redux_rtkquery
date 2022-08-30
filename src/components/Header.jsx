ac6de38f5745552d29679b8a1642c25e8a16368d
NwaguChima <nwaguchima48@gmail.com>
2022-08-31 00:06:41 +0100

    updated header comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCount, getCount } from "../features/posts/postsSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const count = useSelector(getCount);
+
   return (
     <header>
       <h1>Redux Blog</h1>
@@ -17,6 +22,7 @@ const Header = () => {
             <Link to="/user">Users</Link>
           </li>
         </ul>
+        <button onClick={() => dispatch(increaseCount())}>{count}</button>
       </nav>
     </header>
   );

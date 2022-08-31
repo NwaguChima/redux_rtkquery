06ba3d9bc51a38278ffb3c8a54f50c044c3f858f
NwaguChima <nwaguchima48@gmail.com>
2022-08-31 02:25:48 +0100

    updated add reactin buttons

--- src/features/posts/ReactionButtons.jsx
+++ src/features/posts/ReactionButtons.jsx
@@ -1,5 +1,4 @@
-import { useDispatch } from "react-redux";
-import { reactionAdded } from "./postsSlice";
+import { useAddReactionMutation } from "./postsSlice";
 
 const reactionEmoji = {
   thumbsUp: "👍",
@@ -10,7 +9,7 @@ const reactionEmoji = {
 };
 
 const ReactionButtons = ({ post }) => {
-  const dispatch = useDispatch();
+  const [addReaction] = useAddReactionMutation();
 
   const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
     return (
@@ -18,9 +17,13 @@ const ReactionButtons = ({ post }) => {
         key={name}
         type="button"
         className="reactionButton"
-        onClick={() =>
-          dispatch(reactionAdded({ postId: post.id, reaction: name }))
-        }
+        onClick={() => {
+          const newValue = post.reactions[name] + 1;
+          addReaction({
+            postId: post.id,
+            reactions: { ...post.reactions, [name]: newValue },
+          });
+        }}
       >
         {emoji} {post.reactions[name]}
       </button>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -137,6 +137,7 @@ export const {
   useAddNewPostMutation,
   useUpdatePostMutation,
   useDeletePostMutation,
+  useAddReactionMutation,
 } = extendedApiSlice;
 
 export const selectPostsResult = extendedApiSlice.endpoints.getPosts.select();

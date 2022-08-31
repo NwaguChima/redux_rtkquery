e5c70a4c69583e7312947c4cf89c0351f3153aaa
NwaguChima <nwaguchima48@gmail.com>
2022-08-31 01:30:08 +0100

    get post endpoint setup

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,92 +1,38 @@
 import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
+import { apiSlice } from "../api/apiSlice";
 
 const postAdapter = createEntityAdapter({
   sortComparer: (a, b) => b.date.localeCompare(a.date),
 });
 
-const initialState = postAdapter.getInitialState({
-  status: "idle", // idle, loading, succeeded, failed
-  error: null,
-  count: 0,
-});
+const initialState = postAdapter.getInitialState();
 
-export const postsSlice = createSlice({
-  name: "posts",
-  initialState,
-
-  reducers: {
-    reactionAdded: (state, action) => {
-      const { postId, reaction } = action.payload;
-      const existingPost = state.entities[postId];
-      if (existingPost) {
-        existingPost.reactions[reaction]++;
-      }
-    },
-    increaseCount: (state, action) => {
-      state.count = state.count + 1;
-    },
-  },
-  extraReducers(builder) {
-    builder
-      .addCase(fetchPosts.pending, (state, action) => {
-        state.status = "loading";
-      })
-      .addCase(fetchPosts.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        // adding date and reactions to the posts
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "posts",
+      transformResponse: (responseData) => {
         let min = 1;
-        let loadedPosts = action.payload.map((post) => {
-          post.date = sub(new Date(), { minutes: min++ }).toISOString();
-          post.reactions = {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0,
-          };
+        const loadedPosts = responseData.map((post) => {
+          if (!post?.data)
+            post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          if (!post?.reaction)
+            post.reaction = {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            };
+
           return post;
         });
 
-        // add any fetched posts to the array of posts
-        postAdapter.upsertMany(state, loadedPosts);
-      })
-      .addCase(fetchPosts.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      })
-      .addCase(addNewPost.fulfilled, (state, action) => {
-        action.payload.userId = Number(action.payload.userId);
-        action.payload.date = new Date().toISOString();
-        action.payload.reactions = {
-          thumbsUp: 0,
-          wow: 0,
-          heart: 0,
-          rocket: 0,
-          coffee: 0,
-        };
-        postAdapter.addOne(state, action.payload);
-      })
-      .addCase(updatePost.fulfilled, (state, action) => {
-        if (!action.payload?.id) {
-          console.log("Updae could not complete");
-          console.log(action.payload);
-          return;
-        }
-
-        action.payload.date = new Date().toISOString();
-        postAdapter.upsertOne(state, action.payload);
-      })
-      .addCase(deletePost.fulfilled, (state, action) => {
-        if (!action.payload?.id) {
-          console.log("Delete could not complete");
-          console.log(action.payload);
-          return;
-        }
-        const { id } = action.payload;
-        postAdapter.removeOne(state, id);
-      });
-  },
+        return postAdapter.setAll(initialState, loadedPosts);
+      },
+    }),
+  }),
 });
 
 // getSelectors creates these selectors and we rename them with aliases using destructuring
